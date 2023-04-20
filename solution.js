2cab45794285f3fdb85dc405a6788338109af135
Laura Wu <leemaiwu@gmail.com>
2023-04-20 09:29:16 -0600

    Added one line solution using math.abs

--- solution.js
+++ solution.js
@@ -11,3 +11,13 @@ function makeNegative(num) {
 console.log(makeNegative(1)) // -1
 console.log(makeNegative(0)) // 0
 console.log(makeNegative(-1)) // -1
+
+
+// Alternative Solution
+
+
+const makeNeg = (num) => -Math.abs(num)
+
+console.log(makeNeg(1)) // -1
+console.log(makeNeg(0)) // 0
+console.log(makeNeg(-1)) // -1
